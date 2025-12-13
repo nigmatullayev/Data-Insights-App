0b7ab0670ce5e830de1210d02a15a75e5c775d85
nigmatullayev <ahmadnigmatullayev@gmail.com>
2025-12-14 00:42:49 +0500

    write model file and create db file

---
+++ app/db/models.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column,Integer,String,Float,DateTime,ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+from datetime import datetime
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+class Order(Base):
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    product = Column(String)
+    amount = Column(Float)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User")
+
+
+class Sale(Base):
+    __tablename__ = "sales"
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer, ForeignKey("orders.id"))
+    revenue = Column(Float)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    order = relationship("Order")
--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from app.db.database import Base, engine
+from app.db import models
 
 app = FastAPI(title="Data Insights App")
+Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 def root():
